9e085df21600a1dcd55c7fbbb3c81fd1ba5d2d26
guilherme <guile.hm@hotmail.com>
2020-05-27 23:28:49 -0300

    create variables functions

--- src/hello.go
+++ src/hello.go
@@ -4,6 +4,22 @@ import (
 	"fmt"
 )
 
+func variablesInt() {
+	var x int = 5
+	var y int = 10
+	var sum int = x + y
+	fmt.Println(sum)
+}
+
+func mixTypeVariables() {
+	x := 2
+	y := 4
+	sum := x + y
+	fmt.Println(sum)
+}
+
 func main() {
 	fmt.Println("Hello, World!")
+	variablesInt()
+	mixTypeVariables()
 }
