af940a24a2e952f800b9e1e7beea51e929dfa901
guilherme <guile.hm@hotmail.com>
2020-05-27 23:31:36 -0300

    create condition

--- src/hello.go
+++ src/hello.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+func main() {
+	fmt.Println("Hello, World!")
+	variablesInt()
+	mixTypeVariables()
+	condition()
+}
+
 func variablesInt() {
 	var x int = 5
 	var y int = 10
@@ -18,8 +25,13 @@ func mixTypeVariables() {
 	fmt.Println(sum)
 }
 
-func main() {
-	fmt.Println("Hello, World!")
-	variablesInt()
-	mixTypeVariables()
+func condition() {
+	x := 5
+	if x > 5 {
+		fmt.Println("more than 5")
+	} else if x < 2 {
+		fmt.Println("less than 2")
+	} else {
+		fmt.Println("default")
+	}
 }
