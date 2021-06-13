ad49dd29a1a51f1996b12fab8b728746dfe11110
guilherme <guile.hm@hotmail.com>
2021-06-13 11:51:28 -0300

    create helper function

--- hello_test.go
+++ hello_test.go
@@ -4,22 +4,24 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
 	t.Run("saying hello to people", func(t *testing.T) {
 
 		got := Hello("world")
 		want := "Hello, world"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
