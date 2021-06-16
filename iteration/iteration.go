49360ac1ec883d432ac6829bfcce953f98abe7f3
guilherme <guile.hm@hotmail.com>
2021-06-15 21:06:51 -0300

    improve code

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -5,7 +5,7 @@ import "fmt"
 func Repeat(character string) string {
 	var repeated string
 	for i := 0; i < 4; i++ {
-		repeated = repeated + character
+		repeated += character
 	}
 	return repeated
 }
