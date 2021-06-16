a383607e4dd0c3c99697f09cab5a69c1aea24666
guilherme <guile.hm@hotmail.com>
2021-06-15 21:07:58 -0300

    create variable to store repeat count

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
+const repeatCount = 4
+
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 4; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
 	return repeated
